3a4d171f55e84ca9a977284d0e3f444db49fb45c
arbazsiddiqui <arbaz00@gmail.com>
2018-05-28 13:26:02 +0530

    implement dll

--- src/linked-lists/doubly-linked-list.js
+++ src/linked-lists/doubly-linked-list.js
@@ -3,18 +3,55 @@ class LinkedList {
 		this.head = null;
 	}
 
-	pushToEnd(value){
+	pushToEnd(value) {
 		const newNode = new Node(value);
 		let temp = this.head;
-		if(!this.head){
+		if (!this.head) {
 			this.head = newNode;
 			return this.head;
 		}
-		while(temp.next){
+		while (temp.next) {
 			temp = temp.next
 		}
 		temp.next = newNode;
 		newNode.prev = temp;
+		return this.head;
+	}
+
+	pushToStart(value) {
+		const newNode = new Node(value);
+		newNode.next = this.head;
+		this.head = newNode;
+		return this.head;
+	}
+
+	getAtIndex(index) {
+		let temp = this.head;
+		let count = 0;
+		while (temp.next && count < index) {
+			temp = temp.next;
+			count += 1;
+		}
+		if (count === index) {
+			return temp
+		}
+		return null
+	}
+
+	deleteAtIndex(index) {
+		let temp = this.head;
+		let count = 0;
+		while (temp.next && count < index) {
+			temp = temp.next;
+			count += 1;
+		}
+		if (count === index) {
+			temp.prev.next = temp.next;
+			temp.next.prev = temp.prev;
+			temp = null;
+		} else {
+			throw new Error("Cant delete on invalid index")
+		}
 		return this.head;
 	}
 }
@@ -26,3 +63,5 @@ class Node {
 		this.prev = null;
 	}
 }
+
+module.exports = LinkedList;
