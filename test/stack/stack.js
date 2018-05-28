663f6e7a828bab82ff9b8ad7e18675e65a81c6c7
arbazsiddiqui <arbaz00@gmail.com>
2018-05-28 13:59:04 +0530

    test for stack

---
+++ test/stack/stack.js
@@ -0,0 +1,31 @@
+const test = require('ava');
+const Stack = require('../../src/stack/stack');
+
+test('initializes a stack correctly', t => {
+	const stack = new Stack();
+	t.deepEqual(stack.items, [])
+});
+
+test('pushed to a stack correctly', t => {
+	const stack = new Stack();
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+	t.deepEqual(stack.items, [1, 2, 3])
+});
+
+test('pops from a stack correctly', t => {
+	const stack = new Stack();
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+	t.is(stack.pop(), 3)
+});
+
+test('peeks from a stack correctly', t => {
+	const stack = new Stack();
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+	t.is(stack.peek(), 3)
+});
