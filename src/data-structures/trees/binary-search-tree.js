af3e67dd13e4fea81d94c8aaafdab9ad3f1d74f9
arbazsiddiqui <arbaz00@gmail.com>
2018-05-29 19:06:50 +0530

    min and max nodes

--- src/data-structures/trees/binary-search-tree.js
+++ src/data-structures/trees/binary-search-tree.js
@@ -1,26 +1,26 @@
 class BST {
-	constructor(){
+	constructor() {
 		this.root = null;
 	}
 
-	insert(value){
+	insert(value) {
 		const newNode = new Node(value);
-		if(!this.root){
+		if (!this.root) {
 			this.root = newNode;
 			return this.root
 		}
 		this.insertNode(this.root, newNode)
 	}
 
-	insertNode(node, newNode){
-		if(node.value > newNode.value){
-			if(!node.left){
+	insertNode(node, newNode) {
+		if (node.value > newNode.value) {
+			if (!node.left) {
 				node.left = newNode
 			} else {
 				this.insertNode(node.left, newNode)
 			}
 		} else {
-			if(!node.right){
+			if (!node.right) {
 				node.right = newNode
 			} else {
 				this.insertNode(node.right, newNode)
@@ -29,12 +29,12 @@ class BST {
 		return this.root;
 	}
 
-	preOrder(node){
+	preOrder(node) {
 		return this.preOrderCollect(node, []);
 	}
 
-	preOrderCollect(node, res){
-		if(node){
+	preOrderCollect(node, res) {
+		if (node) {
 			res.push(node.value);
 			this.preOrderCollect(node.left, res);
 			this.preOrderCollect(node.right, res);
@@ -42,12 +42,12 @@ class BST {
 		return res
 	}
 
-	inOrder(node){
+	inOrder(node) {
 		return this.inOrderCollect(node, []);
 	}
 
-	inOrderCollect(node, res){
-		if(node){
+	inOrderCollect(node, res) {
+		if (node) {
 			this.inOrderCollect(node.left, res);
 			res.push(node.value);
 			this.inOrderCollect(node.right, res);
@@ -55,22 +55,46 @@ class BST {
 		return res
 	}
 
-	postOrder(node){
+	postOrder(node) {
 		return this.postOrderCollect(node, [])
 	}
 
-	postOrderCollect(node, res){
-		if(node){
+	postOrderCollect(node, res) {
+		if (node) {
 			this.postOrderCollect(node.left, res);
 			this.postOrderCollect(node.right, res);
 			res.push(node.value)
 		}
 		return res
 	}
+
+	minNode(){
+		return this.minNodeIterator(this.root)
+	}
+
+	minNodeIterator(node){
+		if(!node)
+			return null;
+		if(node.left)
+			return this.minNodeIterator(node.left);
+		return node.value
+	}
+
+	maxNode(){
+		return this.maxNodeIterator(this.root)
+	}
+
+	maxNodeIterator(node){
+		if(!node)
+			return null;
+		if(node.right)
+			return this.maxNodeIterator(node.right);
+		return node.value
+	}
 }
 
-class Node{
-	constructor(value){
+class Node {
+	constructor(value) {
 		this.value = value;
 		this.left = null;
 		this.right = null;
